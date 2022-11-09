334f50401beb60a5b1fe05d0d45d16c3e7554860
John Beckett <yorkshireflatcap@gmail.com>
2022-11-09 11:01:41 +0000

    Add project files.

---
+++ Test/2iTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class ITwoTest
+    {
+        // Assumptions:
+        // 4 duplicate numbers consists of 2 sets of 2, or 4 x dublicate numbers; n-1 duplicates to be removed
+        // Return string length < original length
+        // number of elements > 10 will be removed from the end
+
+
+        private int[] _range = new int[10];
+
+        public ITwoTest(int[] range)
+        {
+            
+            if (range.Length > 10)
+            {
+                range = range.Take(10).ToArray();
+            }
+
+            // Check that all values are less than 100
+
+            _range = range.Where(x => x <= 100).Select(x=>x).ToArray();
+        }
+
+        public IEnumerable<int> GetOrderedSet()
+        {
+            var distinct = _range.Select(x => x).Distinct().ToArray();
+            Array.Sort(distinct);
+            return distinct;
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Test;
+
+class Solution
+{
+
+    public static void Main(string[] args)
+    {
+        int[] range = new int[] { 100, 54, 1, 1, 8, 8, 69, 38, 28, 14, 100, 54, 1, 1, 8, 8, 69, 38, 28, 14 };
+        var t = new ITwoTest(range).GetOrderedSet();
+
+    }
+}
